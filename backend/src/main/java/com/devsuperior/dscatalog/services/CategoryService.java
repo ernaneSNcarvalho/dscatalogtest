7fcc4b549336a1e36001ee616f19520a9368bae0
ernane <ernanecarvalho89@gmai.com>
2023-04-26 18:20:50 -0300

    projeto atualizado

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,5 +1,15 @@
 package com.devsuperior.dscatalog.services;
 
-public class CategoryService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.devsuperior.dscatalog.entities.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	
 }
